05e8985bb098dd60c022bc0ad157cf89e0c31d79
יעל יצחקי <100yaeli@gmail.com>
2022-05-11 20:10:03 +0300

    sub2

---
+++ static/JSpersonal.js
@@ -0,0 +1,8 @@
+
+
+function myFunction(){
+    document.getElementById("P1").innerHTML="wow!!!!!";
+    document.getElementById("kelev").src= "https://images.rawpixel.com/image_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvczY5LWFrZTk3NTMtYWUtbC5qcGc.jpg?s=WfotuHZ0zEdp7UL7BM9twAVRdzxlV1fSXHOBwDQZMhI";
+
+
+}
